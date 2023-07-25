22dd77236b600d2965f00ab18a283acacef4a275
Ahadul Islam Sakib <ahad.i.sakib@gmail.com>
2023-07-25 22:29:24 +0600

    se2c

--- assignment.js
+++ assignment.js
@@ -1,1 +1,55 @@
 //https://github.com/ahad77/assignment3
+
+//Problem: 1
+function kilometerToMeter(km){
+    var meter =0;
+    meter =  km*1000;
+    return meter;
+}
+var count = kilometerToMeter(5);
+console.log(count);
+
+//Problem: 2
+function budgetCalculator()
+
+//Problem: 3
+function hotelCost(night){
+    var cost =0;
+    if(night<=10){
+        cost = night*100;
+    }
+    else if(night<=20){
+        var firstphase =10*100;
+        var remainingnight= night-10;
+        var secondphase = remainingnight*80;
+        cost = firstphase + secondphase;
+    }
+    else{
+        var firstphase =10*100;
+        var secondphase = 10*80;
+        var remainingnight= night-20;
+        var thirdphase = remainingnight*50;
+        cost = firstphase + secondphase + thirdphase;
+    }
+    return cost;
+}
+var totalcost = hotelCost(11);
+console.log(totalcost);
+
+
+//Problem: 4
+function megaFriend(arra)
+{
+  var max_name = arra[0].length;
+  var result = arra[0];
+  for (var i = 1; i < arra.length; i++) {
+      var maxi = arra[i].length;
+      if (maxi > max_name) {
+          result = arra[i];
+          max_name = maxi;
+      }
+  }
+  return result;
+}
+var lname = megaFriend(['ahad', 'lisan', 'jawad','Miftah','kaif']);
+console.log(lname);
